9d7265bc93a68f18057b6f151f4ec0d458597ecd
whohensee <106775295+whohensee@users.noreply.github.com>
2025-07-09 09:52:29 -0700

    Add basic animation for points

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const gameData = {
   score: 0,
 };
 
+const balls = new Set();
+
 window.onload = function () {
   board = document.getElementById("board");
   context = board.getContext("2d");
   console.log("this fired on window.onload");
   updateScore();
   setupButtonArea();
+  requestAnimationFrame(update);
 };
 
 function updateScore() {
@@ -31,7 +34,63 @@ function setupButtonArea() {
   btnarea.appendChild(ptbtn);
 }
 
+function update(timestamp) {
+  // move
+  move(timestamp);
+  // draw
+  draw();
+  // request animation frame recursively
+  console.log("aaaa");
+  requestAnimationFrame(update);
+}
+
 function pointbutton() {
   gameData.score += 1;
   updateScore();
+  addBall();
+  console.log(balls);
+}
+
+class Ball {
+  constructor(image, x, y, width, height) {
+    this.image = image;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.velocity = (Math.random() * 4 + 8) / 1000;
+    this.timestamp = undefined;
+  }
+}
+
+function move(timestamp) {
+  for (let ball of balls.values()) {
+    if (ball.timestamp == undefined) {
+      ball.timestamp = timestamp;
+    }
+    ball.y += ball.velocity * (timestamp - ball.timestamp);
+    ball.timestamp = timestamp;
+    if (ball.y > board.height) {
+      balls.delete(ball)
+    }
+  }
+}
+
+function draw() {
+  context.clearRect(0, 0, board.width, board.height);
+  for (let ball of balls.values()) {
+    context.fillStyle = "white";
+    context.fillRect(ball.x, ball.y, ball.width, ball.height);
+  }
+}
+
+function addBall() {
+  const ball = new Ball(
+    null,
+    Math.random() * board.width,
+    (Math.random() * board.height) / 8,
+    4,
+    4,
+  );
+  balls.add(ball);
 }
